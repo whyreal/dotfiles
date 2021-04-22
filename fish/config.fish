164a1f0523b79ca8c8f95a64753152fa09010940
y_real <y_real@qq.com>
2021-04-22 16:45:11 +0800

    mv command `s` to config.sh

--- fish/config.fish
+++ fish/config.fish
@@ -119,4 +119,19 @@ function z
     end
 end
 
+function s
+    if test -z $argv[1]
+        set -l host (command cat \
+        (command awk '/^[ \t]*[Hh]ost/ && $2 != "*" {for (i = 2; i <= NF; i++) print $i}' ~/.ssh/config /etc/ssh/ssh_config | psub) \
+        (command awk '/^[a-z1-9]/{print $1}' /etc/hosts ~/.ssh/known_hosts | psub) \
+        | sort -u | fzf --reverse --history="$HOME/.fzf.history")
+
+        if test -n "$host"
+            ssh $host
+        end
+    else
+        ssh $argv
+    end
+end
+
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
