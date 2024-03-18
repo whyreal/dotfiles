12f8abfeb18ccde7f36fe15cbe3e92ddd12a5f84
whyreal <y_real@qq.com>
2024-03-18 17:52:13 +0800

    update

--- scripts/x.check_https_cert.js
+++ scripts/x.check_https_cert.js
@@ -11,25 +11,26 @@ import './types.js'
   * @returns {Promise<CheckResult>}
   */
 function check_cert(dn) {
-  var options = {
-    host: dn,
-    port: 443,
-    method: 'GET'
-  };
-
+  var result = [dn]
   return new Promise((resolve, reject) => {
+    var options = {
+      host: dn,
+      port: 443,
+      method: 'GET',
+      checkServerIdentity: function (_, cert) {
+        const ca = cert.issuer.O
+        const valid_to = new Date(cert.valid_to)
+        result.push(
+          `${ca}`,
+          Math.floor((valid_to.getTime() - Date.now()) / 24 / 3600 / 1000)
+        )
+      }
+    };
 
     var req = https.request(options, function (res) {
       res.on('data', (_) => {});
-
-      const cert = res.socket.getPeerCertificate();
-      const ca = cert.issuer.O
-      const valid_to = new Date(cert.valid_to)
-      resolve([
-        `${dn}`,
-        `${ca}`,
-        Math.floor((valid_to.getTime() - Date.now()) / 24 / 3600 / 1000),
-        res.socket.remoteAddress])
+      result.push(res.socket.remoteAddress)
+      resolve(result)
     });
     req.on("error", (_) => {
       reject(`${dn}`)
@@ -41,25 +42,24 @@ function check_cert(dn) {
 
 //----------- main -------------
 
-const argv = process.argv.slice(2)
-var domains = []
-
-if (argv.length > 0) {
-  domains = argv
-} else {
-  domains = fs.readFileSync(project_root + "/check_https_cert_dn.txt").toString().split(/\r?\n/)
-  domains = domains.filter(d => {
-    return d.trim().length > 0 && !d.startsWith("#")
+const domains = process.argv.length > 2
+  ? process.argv.slice(2)
+  : fs
+  .readFileSync(project_root + "/check_https_cert_dn.txt")
+  .toString()
+  .split(/\r?\n/)
+  .filter(l => {
+    return l.trim().length > 0 && !l.startsWith("#")
   })
-}
 
-Promise.all(domains.map(d => {
-  return check_cert(d)
-}))
-.then(res => {
-  res.sort((r1, r2) => {
-    r1[3]
-    return r1[2] > r2[2] ? -1 : 1;
-  }).map(r => {console.log(JSON.stringify(r))})
-})
-.catch(e => {console.log(e, ">>>> Error")})
+const print = msg => console.log(msg)
+const printErr = e => console.log(e, ">>>> Error")
+const compareTlsDays = (r1, r2) => r1[2] > r2[2] ? -1 : 1 
+const sortPrint = res => res
+  .sort(compareTlsDays)
+  .map(JSON.stringify)
+  .map(print)
+
+Promise.all(domains.map(check_cert))
+  .then(sortPrint)
+  .catch(printErr)
